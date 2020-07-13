41dc7942f093ce77e80345468dcff79f4ffa8f79
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-07-12 23:37:48 -0300

    Initial Commit Setting Up Firebase

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Http</h1>
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" class="form-control" v-model="user.username" />
+          </div>
+          <div class="form-group">
+            <label>Mail</label>
+            <input type="text" class="form-control" v-model="user.email" />
+          </div>
+          <button class="btn btn-primary" @click="submit">Submit</button>
+        </div>
+      </div>
+    </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.user);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
